e5c7a7f363916af0e6248d961983cba0953abb8f
Ukabu <emmanuel@pirsch.org>
2018-10-17 15:18:30 -0400

    remove unneeded params.yml file

--- ci/set_pipeline.sh
+++ ci/set_pipeline.sh
@@ -3,8 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ALIAS=${1:-emerald-squad}
-PARAMS=${2:-$DIR/params.yml}
-PIPELINE_NAME=${3:-sonar-scanner}
+PIPELINE_NAME=${2:-sonar-scanner}
 
-fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c $DIR/pipeline.yml -l "${PARAMS}" --non-interactive 
+fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c $DIR/pipeline.yml --non-interactive 
 
